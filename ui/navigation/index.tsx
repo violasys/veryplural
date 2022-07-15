4bcb285d0e15af70eb2818ece9df4d6a66794382
Viola <violasandpronouns@gmail.com>
2022-07-14 20:00:47 -0400

    remove unneeded header from members screen

--- ui/navigation/index.tsx
+++ ui/navigation/index.tsx
@@ -88,23 +88,8 @@ function BottomTabNavigator() {
         name="Members"
         component={MembersScreen}
         options={({ navigation }: RootTabScreenProps<"Members">) => ({
-          title: "Members",
+          headerShown: false,
           tabBarIcon: ({ color }) => <TabBarIcon name="people" color={color} />,
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate("Modal")}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}
-            >
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
         })}
       />
       <BottomTab.Screen
